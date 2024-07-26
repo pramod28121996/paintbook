23525f5bae233ffa3cbe13ed5582e349e01302ba
Pramod <pramodvariyawork@gmail.com>
2024-07-26 16:38:10 +0530

    add privacy page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Privacy from "./components/privacy";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Privacy />} />
+          <Route path="/privacy" element={<Privacy />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/components/privacy.jsx
@@ -0,0 +1,369 @@
+const Privacy = () => {
+  return (
+    <div>
+      <center>
+        <h1 style={{ marginBottom: 0 }}>Privacy Policy</h1>
+      </center>
+      <pre
+        style={{
+          fontVariantLigatures: "normal",
+          orphans: 2,
+          overflowWrap: "break-word",
+          textDecorationThickness: "initial",
+          whiteSpace: "pre-wrap",
+          widows: 2,
+          padding: "20px",
+        }}
+      >
+        <p style={{ fontFamily: "-webkit-standard", whiteSpace: "normal" }}>
+          <span>
+            Your privacy is important to us. Sometimes we need information to
+            provide services that you request, this privacy statement applies to
+            Just app and If you have additional questions or require more
+            information about our Privacy Policy, do not hesitate to Contact
+            through email at&nbsp;
+          </span>
+
+          <b>imcoder2287@gmail.com&nbsp;</b>
+
+          <span>by</span>
+
+          <b>&nbsp;imcoder</b>
+        </p>
+
+        <h2 style={{ fontFamily: "-webkit-standard", whiteSpace: "normal" }} />
+
+        <h2 style={{ fontFamily: "-webkit-standard", whiteSpace: "normal" }}>
+          Personal Information&nbsp;
+        </h2>
+
+        <p style={{ fontFamily: "-webkit-standard", whiteSpace: "normal" }}>
+          We DO NOT collect, store or use any personal information while
+          youvisit, download or upgrade our products.&nbsp;
+          <span
+            face="Roboto, Helvetica, Arial, sans-serif"
+            style={{ backgroundColor: "white" }}
+          >
+            Apps do not access any personal and sensitive user data.
+          </span>
+        </p>
+
+        <h2 style={{ fontFamily: "-webkit-standard", whiteSpace: "normal" }}>
+          <b>Information we get from your use of our services&nbsp;</b>
+        </h2>
+
+        <p style={{ fontFamily: "-webkit-standard", whiteSpace: "normal" }} />
+
+        <p style={{ fontFamily: "-webkit-standard", whiteSpace: "normal" }}>
+          We may collect information about the services that you use and how
+          youuse them, such as when you view and interact with our content. We
+          maycollect device-specific information (such as your hardware
+          model,operating system version, unique device identifiers, device
+          settings,device language and mobile network information). We will not
+          sharethat information with third parties.&nbsp;
+        </p>
+
+        <div
+          style={{
+            WebkitFontSmoothing: "antialiased",
+            backgroundColor: "white",
+            boxSizing: "border-box",
+            fontFamily: "Oxygen",
+            fontSize: "16px",
+            fontVariantLigatures: "normal",
+            textDecorationThickness: "initial",
+            whiteSpace: "normal",
+          }}
+        >
+          <p />
+        </div>
+
+        <h4 style={{ fontFamily: "-webkit-standard", whiteSpace: "normal" }}>
+          <ul>
+            <li>Internet</li>
+          </ul>
+
+          <span style={{ fontWeight: "normal" }}>
+            <div style={{ textAlign: "justify" }}>
+              {"              "}&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+              This permission
+              {"              "}is needed for reading data online.
+            </div>
+
+            <div style={{ textAlign: "justify" }}>
+              {"              "}
+              <br />
+            </div>
+          </span>
+        </h4>
+
+        <h3
+          className="aFEzEb"
+          style={{
+            backgroundColor: "white",
+            border: "0px",
+            boxSizing: "border-box",
+            color: "#202124",
+            fill: "rgb(32, 33, 36)",
+            fontFamily: '"Google Sans", Roboto, Arial, sans-serif',
+            fontFeatureSettings: "inherit",
+            fontKerning: "inherit",
+            fontOpticalSizing: "inherit",
+            fontSize: "1.125rem",
+            fontStretch: "inherit",
+            fontVariantAlternates: "inherit",
+            fontVariantEastAsian: "inherit",
+            fontVariantLigatures: "normal",
+            fontVariantNumeric: "inherit",
+            fontVariantPosition: "inherit",
+            fontVariationSettings: "inherit",
+            lineHeight: "1.5rem",
+            margin: "0px",
+            padding: "0px",
+            stopColor: "rgb(32, 33, 36)",
+            textDecorationColor: "initial",
+            textDecorationStyle: "initial",
+            textDecorationThickness: "initial",
+            verticalAlign: "baseline",
+            whiteSpace: "normal",
+          }}
+        >
+          The Play Families Policy
+        </h3>
+
+        <div>
+          <br />
+        </div>
+
+        <div>
+          <p
+            style={{
+              backgroundColor: "white",
+              boxSizing: "border-box",
+              fontFamily:
+                '"Open Sans", sans-serif, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", "Liberation Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
+              fontVariantLigatures: "normal",
+              marginBottom: "1rem",
+              marginTop: "0px",
+              textDecorationColor: "initial",
+              textDecorationStyle: "initial",
+              textDecorationThickness: "initial",
+              whiteSpace: "normal",
+            }}
+          >
+            <span style={{ fontSize: "x-small" }}>
+              {"              "}
+              <span style={{ color: "#484848" }}>
+                {"                "}We offer some parts of our services,
+                particularly the Apps, to
+                {"                "}children. If a parent or a legal guardian of
+                an underage child
+                {"                "}thinks that their child is using our Apps
+                without their consent
+                {"                "}then they can contact us by writing at&nbsp;
+                {"              "}
+              </span>
+
+              {"              "}
+              <a href="mailto:imcoder2287@gmail.com">
+                {"                "}
+                <span>
+                  {"                  "}
+                  <span style={{ boxSizing: "border-box" }}>imcoder2287@</span>
+
+                  {"                "}
+                </span>
+                {"                "}gmail.com
+                {"              "}
+              </a>
+
+              {"              "}
+              <span style={{ color: "#484848" }}>
+                {"                "}. We will immediately take actions,
+                including removal of
+                {"                "}personal information relating to an underage
+                child, subject to
+                {"                "}proof of identification to prevent any
+                wrongful removal of
+                {"                "}information.
+                {"              "}
+              </span>
+            </span>
+          </p>
+
+          <p
+            style={{
+              backgroundColor: "white",
+              boxSizing: "border-box",
+              color: "#484848",
+              fontFamily:
+                '"Open Sans", sans-serif, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", "Liberation Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
+              fontVariantLigatures: "normal",
+              marginBottom: "1rem",
+              marginTop: "0px",
+              textDecorationColor: "initial",
+              textDecorationStyle: "initial",
+              textDecorationThickness: "initial",
+              whiteSpace: "normal",
+            }}
+          >
+            <span style={{ fontSize: "x-small" }}>
+              {"              "}Parents of legal guardians of children have
+              certain rights that
+              {"              "}they may exercise by directly getting in touch
+              with us via our
+              {"              "}email address&nbsp;info@bebi.family. These
+              rights include:
+            </span>
+          </p>
+
+          <ul
+            style={{
+              backgroundColor: "white",
+              boxSizing: "border-box",
+              color: "#484848",
+              fontFamily:
+                '"Open Sans", sans-serif, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", "Liberation Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
+              fontVariantLigatures: "normal",
+              marginBottom: "1rem",
+              marginTop: "0px",
+              paddingLeft: "2rem",
+              textDecorationColor: "initial",
+              textDecorationStyle: "initial",
+              textDecorationThickness: "initial",
+              whiteSpace: "normal",
+            }}
+          >
+            <li style={{ boxSizing: "border-box" }}>
+              {"              "}
+              <span style={{ fontSize: "x-small" }}>
+                {"                "}Right to know what information we collect
+                about children and how
+                {"                "}we process this data;
+                {"              "}
+              </span>
+            </li>
+
+            <li style={{ boxSizing: "border-box" }}>
+              {"              "}
+              <span style={{ fontSize: "x-small" }}>
+                {"                "}Right to request removal or rectification of
+                information
+                {"                "}relating to a child;
+                {"              "}
+              </span>
+            </li>
+
+            <li style={{ boxSizing: "border-box" }}>
+              {"              "}
+              <span style={{ fontSize: "x-small" }}>
+                {"                "}Right to withdraw consent of the collection
+                of personal data of
+                {"                "}a child.
+                {"              "}
+              </span>
+            </li>
+          </ul>
+        </div>
+
+        <h3
+          className="aFEzEb"
+          style={{
+            backgroundColor: "white",
+            border: "0px",
+            boxSizing: "border-box",
+            color: "#202124",
+            fill: "rgb(32, 33, 36)",
+            fontFamily: '"Google Sans", Roboto, Arial, sans-serif',
+            fontFeatureSettings: "inherit",
+            fontKerning: "inherit",
+            fontOpticalSizing: "inherit",
+            fontSize: "1.125rem",
+            fontStretch: "inherit",
+            fontVariantAlternates: "inherit",
+            fontVariantEastAsian: "inherit",
+            fontVariantLigatures: "normal",
+            fontVariantNumeric: "inherit",
+            fontVariantPosition: "inherit",
+            fontVariationSettings: "inherit",
+            lineHeight: "1.5rem",
+            margin: "0px",
+            padding: "0px",
+            stopColor: "rgb(32, 33, 36)",
+            textDecorationColor: "initial",
+            textDecorationStyle: "initial",
+            textDecorationThickness: "initial",
+            verticalAlign: "baseline",
+            whiteSpace: "normal",
+          }}
+        >
+          <span
+            face="Roboto, Helvetica, Arial, sans-serif"
+            style={{
+              fontSize: "small",
+              fontWeight: "normal",
+              letterSpacing: "0.0125em",
+            }}
+          >
+            The App has committed to follow the Play Families Policy for this
+            app.
+          </span>
+
+          <span
+            face="Roboto, Helvetica, Arial, sans-serif"
+            style={{
+              fontSize: "small",
+              fontWeight: "normal",
+              letterSpacing: "0.0125em",
+            }}
+          >
+            &nbsp;
+          </span>
+
+          <a
+            href="https://support.google.com/googleplay/android-developer/answer/9893335"
+            style={{
+              border: "0px",
+              boxSizing: "border-box",
+              fill: "rgb(95, 99, 104)",
+              fontFamily: "inherit",
+              fontFeatureSettings: "inherit",
+              fontKerning: "inherit",
+              fontOpticalSizing: "inherit",
+              fontStretch: "inherit",
+              fontStyle: "inherit",
+              fontVariantAlternates: "inherit",
+              fontVariantCaps: "inherit",
+              fontVariantEastAsian: "inherit",
+              fontVariantLigatures: "inherit",
+              fontVariantNumeric: "inherit",
+              fontVariantPosition: "inherit",
+              fontVariationSettings: "inherit",
+              fontWeight: "normal",
+              letterSpacing: "0.0125em",
+              lineHeight: "inherit",
+              margin: "0px",
+              padding: "0px",
+              stopColor: "rgb(95, 99, 104)",
+              verticalAlign: "baseline",
+            }}
+            target="_blank"
+          >
+            See the policy
+          </a>
+        </h3>
+
+        <h2 style={{ fontFamily: "-webkit-standard", whiteSpace: "normal" }}>
+          <br />
+        </h2>
+
+        <h2 style={{ fontFamily: "-webkit-standard", whiteSpace: "normal" }}>
+          WE WILL NOT SHARE ANY INFORMATION FROM YOUR DEVICE WITH THIRD PARTIES.
+        </h2>
+
+        {"      "}
+      </pre>
+    </div>
+  );
+};
+export default Privacy;
